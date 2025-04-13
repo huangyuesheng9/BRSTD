3f0133a772fe60a41875d71e45df3eb585575f38
huangyuesheng9 <huangyuesheng9@gmail.com>
2025-04-13 15:56:00 +0800

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 os.environ["WANDB_MODE"] = "offline"
 if __name__ == '__main__':
     # model = (YOLO("ultralytics/cfg/models/v8/yolov8.yaml"))  # 模型 CA 4
-    model = (YOLO("BRSTD/VIT_bl.yaml"))  # 模型 CA
+    model = (YOLO("BRSTD/yolov8-BRSTD.yaml"))  # 模型 CA
     model.train(**{'cfg': 'ultralytics/cfg/default.yaml'})  # 配置
 #     model = YOLO(r'runs/detect/DOTA/weights/best.pt')  # load a partially trained model
 
