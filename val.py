746ed861895e85980fe12726a79c47682c31f03c
huangyuesheng9 <huangyuesheng9@gmail.com>
2025-05-20 18:53:00 +0800

    Add files via upload

--- val.py
+++ val.py
@@ -6,7 +6,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 if __name__ == '__main__':
     # Load a model
     # model = YOLO('runs\detect/visdrone_n\weights/best.pt')  # load an official model
-    model = YOLO(r'E:\第二篇结果\best.pt')
-    metrics = model.val(data='data/AI-TOD.yaml', iou=0.7, conf=0.001, half=False, device=0, save_json=True, name=r'val\AI-TOD_n')
+    model = YOLO(r'runs\detect\visdrone-s\weights\best.pt')
+    metrics = model.val(data='visdrone.yaml', iou=0.7, conf=0.001, half=False, device=0, save_json=True, name=r'val\visdrone_n')
     # Validate the model
     # metrics = model.val(data='ultralytics/datasets/coco.yaml', iou=0.7, conf=0.001, half=False, device=0, save_json=True)
